cd70e9c33baa27dd950a253dc8c0d4743aca9ae9
Michael Matloob <matloob@golang.org>
2022-03-31 16:17:17 -0400

    remove internal stuff not being used

--- internal/diff/diff.go
+++ internal/diff/diff.go
@@ -7,11 +7,9 @@
 package diff
 
 import (
-	"bytes"
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"runtime"
 )
 
 // Returns diff of two arrays of bytes in diff tool format.
@@ -29,33 +27,12 @@ func Diff(prefix string, b1, b2 []byte) ([]byte, error) {
 	defer os.Remove(f2)
 
 	cmd := "diff"
-	if runtime.GOOS == "plan9" {
-		cmd = "/bin/ape/diff"
-	}
 
 	data, err := exec.Command(cmd, "-u", f1, f2).CombinedOutput()
 	if len(data) > 0 {
 		// diff exits with a non-zero status when the files don't match.
 		// Ignore that failure as long as we get output.
 		err = nil
-	}
-
-	// If we are on Windows and the diff is Cygwin diff,
-	// machines can get into a state where every Cygwin
-	// command works fine but prints a useless message like:
-	//
-	//	Cygwin WARNING:
-	//	  Couldn't compute FAST_CWD pointer.  This typically occurs if you're using
-	//	  an older Cygwin version on a newer Windows.  Please update to the latest
-	//	  available Cygwin version from https://cygwin.com/.  If the problem persists,
-	//	  please see https://cygwin.com/problems.html
-	//
-	// Skip over that message and just return the actual diff.
-	if len(data) > 0 && !bytes.HasPrefix(data, []byte("--- ")) {
-		i := bytes.Index(data, []byte("\n--- "))
-		if i >= 0 && i < 80*10 && bytes.Contains(data[:i], []byte("://cygwin.com/")) {
-			data = data[i+1:]
-		}
 	}
 
 	return data, err
